df50a7d73d0fb0ed1a204c243bfc797fdce14414
CRW405 <134819932+CRW405@users.noreply.github.com>
2025-08-28 14:23:36 -0500

    .

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,24 +1,23 @@
 package homework1;
+
 public class Fibonacci {
     public static void main(String[] args) {
         int n = 10;
-        if (args.length > 0) {
+        if (args.length > 0) 
             n = Integer.parseInt(args[0]);
-        }
 
         int[] sequence = new int[n + 1];
 
         fibonacci(sequence, n);
 
-        for (int i = 0; i < n; i++) {
-            System.out.println(sequence[i]);
+        for (int i : sequence) {
+            System.out.println(i);
         }
     }
 
     public static int fibonacci(int[] sequence, int n) {
-        if (n < 2) {
+        if (n < 2) 
             return sequence[n] = n;
-        }
         return sequence[n] = fibonacci(sequence, n - 1) + fibonacci(sequence, n - 2);
     }
 }
